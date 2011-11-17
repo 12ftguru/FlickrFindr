513ca88d029f56622cf8c58400d45233113274dc
John E. Clark <john@12ftguru.com>
2011-11-17 13:10:38 -0500

    Moved namespace declaration into the app.js file

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
  * This file sets application-wide settings and launches the application when everything has
  * been loaded onto the page. By default we just render the applications Viewport inside the
  * launch method (see app/views/Viewport.js).
- */ 
+ */
 FlickrFindr = new Ext.Application({
     defaultTarget: "viewport",
     name: "FlickrFindr",
@@ -10,3 +10,4 @@ FlickrFindr = new Ext.Application({
         this.viewport = new FlickrFindr.Viewport();
     }
 });
+Ext.namespace('FlickrFindr.view', 'FlickrFindr.model', 'FlickrFindr.store');
