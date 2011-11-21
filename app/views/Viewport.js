cf78076b9bb0dac6fbf26dcf7ecd9ccb17129dc8
Bryan P Johnson <bryan@12ftguru.com>
2011-11-21 15:31:47 -0500

    added tab view

--- app/views/Viewport.js
+++ app/views/Viewport.js
@@ -1,14 +1,28 @@
-FlickrFindr.Viewport = Ext.extend(Ext.Panel, {
+FlickrFindr.Viewport = Ext.extend(Ext.TabPanel, {
   id: 'viewport',
-  layout: 'card',
   fullscreen: true,
-
+  cardSwitchAnimation: 'slide',
+  tabBar: {
+    dock: 'bottom',
+    layout: {
+      pack: 'center'
+    }
+  },
   initComponent: function() {
     Ext.apply(this, {
       dockedItems: [],
       items: [{
-        xtype: 'searchresults'
-      }]
+        xtype: 'searchresults',
+        title: 'Search',
+        iconCls: 'search'
+      },
+              {
+        xtype: 'panel',
+        html: 'Bookmarks go here.',
+        title: 'Bookmarks',
+        iconCls: 'favorites'
+      }
+              ]
     });
 
     FlickrFindr.Viewport.superclass.initComponent.apply(this, arguments);
