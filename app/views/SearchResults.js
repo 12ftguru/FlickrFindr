5682c0ce693027e1c1760b8896a03ce2b63eb2c8
Bryan P Johnson <bryan@12ftguru.com>
2011-11-22 18:59:39 -0500

    more defaults

--- app/views/SearchResults.js
+++ app/views/SearchResults.js
@@ -1,8 +1,4 @@
-FlickrFindr.view.SearchResultTpl = new Ext.XTemplate(
-'<div class="searchresult">',
-'<img src="{[this.getPhotoURL("s", values)]}" height="75" width="75"/>',
-' {title}</div>',
-{
+FlickrFindr.view.SearchResultTpl = new Ext.XTemplate('<div class="searchresult">', '<img src="{[this.getPhotoURL("s", values)]}" height="75" width="75"/>', ' {title}</div>', {
   getPhotoURL: function(size, values) { /* Form a URL based on Flickr's URL specification: http://www.flickr.com/services/api/misc.urls.html */
     size = size || 's';
     var url = 'http://farm' + values.farm + '.static.flickr.com/' + values.server + '/' + values.id + '_' + values.secret + '_' + size + '.jpg';
@@ -33,7 +29,7 @@ FlickrFindr.view.SearchResults = Ext.extend(Ext.Panel, {
             });
           }
         },
-                                                                      {
+                                                                                {
           text: 'Next 25',
           ui: 'forward',
           handler: function() {
@@ -55,6 +51,17 @@ FlickrFindr.view.SearchResults = Ext.extend(Ext.Panel, {
             var geo = new Ext.util.GeoLocation({
               autoUpdate: false
             });
+            var easyparams = {
+              "min_upload_date": dt.format("Y-m-d H:i:s"),
+              "lat": 38.8894504,
+              "lon": -77.0353496,
+              "accuracy": 16,
+              "radius": 10,
+              "radius_units": "km"
+            };
+
+            this.getStore().getProxy().extraParams = Ext.applyIf(this.getStore().getProxy().extraParams, easyparams);
+
             geo.updateLocation(function(geo) {
               if (geo === null) {
                 geo = {
